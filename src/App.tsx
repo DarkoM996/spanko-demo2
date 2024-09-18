791dc462af0e50e9bb18468e8ceff732cf72de88
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-09-18 17:00:57 +0200

    changing meta and adding poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className="w-full h-screen bg-slate-100 text-slate-950">
-      <h2 className="text-5xl">Hello From This React Typescript Website</h2>
+    <div className="w-full h-screen bg-slate-100">
+      <h2 className="text-5xl font-bold">
+        Hello From This React Typescript Website
+      </h2>
     </div>
   );
 };
