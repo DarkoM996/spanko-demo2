913f4c2c49e06ffa56c7e5c2978acc4e092a7573
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-09-18 17:11:49 +0200

    Setting global background color for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="w-full h-screen bg-slate-100">
+    <div className="w-full h-full">
       <h2 className="text-5xl font-bold">
         Hello From This React Typescript Website
       </h2>
