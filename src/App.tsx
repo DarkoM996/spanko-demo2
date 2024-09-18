2e9c943dd8349cb16827838b0b964ecb0ae25b1e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-09-18 16:58:33 +0200

    Extending Tailwind with some initial design specs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className="w-full h-screen bg-slate-100 text-slate-950">App</div>;
+  return (
+    <div className="w-full h-screen bg-slate-100 text-slate-950">
+      <h2 className="text-5xl">Hello From This React Typescript Website</h2>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
