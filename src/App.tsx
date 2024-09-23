e987474c971e325eb40039e07cd16bccc9b61fd2
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-09-23 14:58:27 +0200

    Shadcn Installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
   return (
     <div className="w-full h-full">
       <h2 className="text-5xl font-bold">
         Hello From This React Typescript Website
       </h2>
+      <Button>Click Me</Button>
     </div>
   );
 };
