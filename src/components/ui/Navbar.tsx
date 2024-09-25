49ed94282feadfcbcc0503e7ee6f1a0bba48f24a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-09-25 16:59:30 +0200

    latest

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,16 +1,22 @@
+import { IoMdMenu } from "react-icons/io";
+
 const Navbar = () => {
   return (
-    <header className="relative z-[999] px-6 py-6">
-      <nav className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center bg-slate-200 rounded-full px-6 ">
+    <header className="relative z-[999] px-6 py-6 lg:px-28">
+      <nav className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center  rounded-full px-6 ">
         {/* TypeLogo instead of the actual logo for now */}
         <div className="">
-          <h3 className="text-3xl font-bold">SpankoMattress</h3>
+          <h3 className="text-xl md:text-3xl font-bold">SpankoMattress</h3>
         </div>
-        <ul className="flex gap-4">
+        <ul className="hidden md:flex gap-4">
           <li>Home</li>
           <li>Mattresses</li>
           <li>Contact</li>
         </ul>
+        {/* Toggle Button */}
+        <div className="hidden md:flex">Toggle</div>
+        {/* Menu Button */}
+        <IoMdMenu className="md:hidden text-2xl" />
       </nav>
     </header>
   );
