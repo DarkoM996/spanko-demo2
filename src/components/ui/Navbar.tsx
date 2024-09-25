433680efaccd0aa28ce889028a14df0495e27d6a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-09-25 21:12:00 +0200

    fixed color

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import { IoMdMenu } from "react-icons/io";
 const Navbar = () => {
   return (
     <header className="relative z-[999] px-6 py-6 lg:px-28">
-      <nav className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center  rounded-full px-6 ">
+      <nav className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center bg-primary-foreground dark:bg-slate-900  rounded-full px-6 ">
         {/* TypeLogo instead of the actual logo for now */}
         <div className="">
           <h3 className="text-xl md:text-3xl font-bold">SpankoMattress</h3>
